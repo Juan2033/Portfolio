b714c387808b8f5c99f2e175420b74373974d88f
Juan2033 <juancabac@hotmail.com>
2026-01-21 16:52:35 -0500

    Nuevo cambio

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ function descargarCV() {
     link.download = 'CV_Juan_Ballesteros.pdf';
     link.click();
 }
+
+const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        
+        // Paginación
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        
+        // Navegación con flechas
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+        // Responsive breakpoints
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        },
+      });
